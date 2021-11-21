835e97ae1c9a4088f0c1761a4ce6d4b32a3a4ac8
Matt Cosand <matt@cosand.net>
2021-11-21 10:08:36 -0800

    Mission credit for CCs script

--- award-mission-quals.js
+++ award-mission-quals.js
@@ -1,6 +1,7 @@
 import { d4hClient } from './d4h.js';
-import { subDays, subMonths, isAfter } from 'date-fns';
+import { subMonths, isAfter, addMonths, parseISO } from 'date-fns';
 
+const NOTE_PREFIX = 'cc-script:';
 
 const rules = [
   { name: 'ESAR Field', test: signin => signin?.role?.bundle == 'ESAR' && signin.role.title.includes("(Mission)"), needHours: 30, cutoff: subMonths(new Date(), 12) },
@@ -9,15 +10,21 @@ const rules = [
 
 const cutoff = new Date(Math.min.apply(this, rules.map(r => r.cutoff.getTime())));
 
-async function body() {
-  let signins = [];
+async function getChunkedList(name, url) {
+  let list = [];
   let chunk = [];
-  console.log(`Getting sign-ins since ${cutoff}...`);
   do {
-    chunk = (await d4hClient.get(`team/attendance?limit=250&offset=${signins.length}&activity=incident&status=attending&sort=date:desc&after=${cutoff.toJSON()}`)).data.data;
-    signins = [...signins, ...chunk ];
-    console.log(`  ${chunk[0].date}  ${chunk[chunk.length - 1].date}`);
+    chunk = (await d4hClient.get(`${url}${url.includes('?') ? '&' : '?'}limit=250&offset=${list.length}`)).data.data;
+    list = [ ...list, ...chunk ];
+    console.log(`${name}: ${list.length}`);
   } while (chunk.length >= 250);
+
+  return list;
+}
+
+async function body() {
+  const memberLookup = (await getChunkedList('members', `team/members?include_details=true`)).reduce((accum, cur) => ({ ...accum, [cur.id]: cur}), {});
+  const signins = await getChunkedList('rosters', `team/attendance?activity=incident&status=attending&sort=date:desc&after=${cutoff.toJSON()}`);
 
   let members = rules.reduce((accum, cur) => ({ ...accum, [cur.name]: {}}), {});
 
@@ -35,7 +42,7 @@ async function body() {
         existing.date = signin.date;
         existing.name = signin.member.name;
         if (existing.minutes >= rule.needHours * 60) {
-          awards.push({ award: rule, member: signin.member, date: signin.date});
+          awards.push({ award: rule, member: memberLookup[signin.member.id], date: signin.date});
         }
       }
       members[rule.name][signin.member.id] = existing;
@@ -43,7 +50,66 @@ async function body() {
     }
   }
 
-  awards.forEach(a => console.log(`${a.date} - ${a.member.name} - ${a.award.name}`));
+  awards = awards;//.filter(f => f.member.name == 'Cosand, Matt');
+  for (let i = 0; i< awards.length; i++) {
+    const a = awards[i];
+    console.log(`${a.date} - ${a.member.name} - ${a.award.name}`);
+    
+    let dirty = false;
+    //const qualsResponse = await d4hClient.get(`team/members/${a.member.id}/qualification-awards`);
+
+    const noteLines = (a.member.notes?.replace(/\\\r/g, '\r').replace(/\\\n/g, '\n') ?? '').split('\r\n') ?? [];
+    const otherNotes = noteLines.filter(l => !l.startsWith(NOTE_PREFIX));
+
+    const scriptNoteLine = noteLines.filter(l => l.startsWith(NOTE_PREFIX))[0]?.substr(10)?.replace(/\\"/g, '"') ?? '{}';
+    let noteInfo;
+    try {
+      console.log(`**${scriptNoteLine}**`);
+      noteInfo = JSON.parse(scriptNoteLine);
+    } catch (err) { 
+      console.log(`Failed to parse cc-script note for ${a.member.name}: **${scriptNoteLine}** ${err}`);
+      noteInfo = {};
+    }
+
+
+    const quals = [
+      27521, // Radio.PE
+      27529, // Search Techniques
+      27523, // Navigation
+      27520, // Survival
+      27533, // Rescue Techniques
+      27525, // GPS
+    ];
+    for (let j=0; j < quals.length; j++) {
+      const qid = quals[j];
+
+      if (noteInfo[qid] && !isAfter(parseISO(a.date), addMonths(parseISO(noteInfo[qid]), 12))) {
+        console.log(`${a.member.name} already has qualification ${qid} from script on ${noteInfo[qid]}`);
+        continue;
+      }
+
+      const params = new URLSearchParams()
+      params.append('member_id', a.member.id)
+      params.append('start_date', a.date)
+      const response = await d4hClient.post(`team/qualifications/${qid}/qualified-members`, params, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' }});
+      noteInfo[qid] = a.date.substr(0, 10);
+      dirty = true;
+    }
+
+    if (noteLines.filter(l => l.startsWith(NOTE_PREFIX)).length > 1) {
+      console.log('  removing extra note line(s)')
+      dirty = true;
+    }
+
+    if (dirty) {
+      console.log('  updating notes')
+      const newNotes = [ ...otherNotes, `${NOTE_PREFIX}${JSON.stringify(noteInfo)}`].join('\r\n');
+      const params = new URLSearchParams();
+      params.append('notes', newNotes);
+    
+      await d4hClient.put(`team/members/${a.member.id}`, params, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' }});
+    }
+  }
   console.log(awards.length)
 }
 
